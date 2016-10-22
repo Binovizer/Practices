7f8b0790f3ee9ae1bdf73b0cf02d46a898dae500
Nadeem <mohd.nadeem3464@gmail.com>
2016-10-22 23:10:56 +0530

    ads

---
+++ GeeksForGeeks/src/Basic/FindTheRemainder.java
@@ -0,0 +1,25 @@
+package Basic;
+
+import java.util.Scanner;
+
+/**
+ * @author Mohd Nadeem(mohd.nadeem3464@gmail.com)
+ *
+ */
+public class FindTheRemainder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int t = scan.nextInt();
+		scan.nextLine();
+		while(t-->0){
+			String str = scan.nextLine();
+		}
+		scan.close();
+
+	}
+
+}
