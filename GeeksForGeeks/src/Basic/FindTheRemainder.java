b230c91a89ccaddcb7397a8c5c5337029062acaa
Nadeem <mohd.nadeem3464@gmail.com>
2016-10-22 23:32:16 +0530

    Find the remainder

--- GeeksForGeeks/src/Basic/FindTheRemainder.java
+++ GeeksForGeeks/src/Basic/FindTheRemainder.java
@@ -1,5 +1,6 @@
 package Basic;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -16,7 +17,18 @@ public class FindTheRemainder {
 		int t = scan.nextInt();
 		scan.nextLine();
 		while(t-->0){
+			int d = 7;
 			String str = scan.nextLine();
+			int arr[] = new int[str.length()];
+			arr[0] = 1;
+			for(int i=1;i<str.length();i++){
+				arr[i] = (arr[i-1]*10) % d;
+			}
+			int rem = 0;
+			for(int i = str.length()-1;i>=0;i--){
+				rem = ( rem + Integer.parseInt(str.charAt(i)+"") * arr[str.length()-i-1] ) % d;
+			}
+			System.out.println(rem);
 		}
 		scan.close();
 
